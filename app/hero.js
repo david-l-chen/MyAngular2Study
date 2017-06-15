045e73900d373bd38fc8b007421b83f8d96232be
david.chen <9949363@qq.com>
2017-06-16 00:08:37 +0800

    new file:   app/app.component.js 	new file:   app/app.component.js.map 	new file:   app/app.component.ts 	new file:   app/app.module.js 	new file:   app/app.module.js.map 	new file:   app/app.module.ts 	new file:   app/hero-detail.component.js 	new file:   app/hero-detail.component.js.map 	new file:   app/hero-detail.component.ts 	new file:   app/hero.js 	new file:   app/hero.js.map 	new file:   app/hero.service.js 	new file:   app/hero.service.js.map 	new file:   app/hero.service.ts 	new file:   app/hero.ts 	new file:   app/mock-heroes.js 	new file:   app/mock-heroes.js.map 	new file:   app/mock-heroes.ts 	new file:   index.html 	new file:   main.js 	new file:   main.js.map 	new file:   main.ts 	new file:   styles.css 	new file:   systemjs-angular-loader.js 	new file:   systemjs.config.extras.js 	new file:   systemjs.config.js 	new file:   tsconfig.json

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by David on 2017/6/15.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by David on 2017/6/15.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
